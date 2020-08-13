44c2a554e03be01f6c38b1b30b7723dc00259d34
UserGLA <tigr1488@yandex.ru>
2020-08-14 00:26:39 +0700

    Поправил ошибки

--- src/main.js
+++ src/main.js
@@ -274,54 +274,65 @@ const createEventEdit = () => {
   );
 };
 
-const createSiteTripDays = () => {
+const createTripDaysList = () => {
+  return (
+    `<ul class="trip-days"></ul>`
+  );
+};
+
+const createTripDays = () => {
+  return (
+    `<li class="trip-days__item  day">
+    <div class="day__info">
+      <span class="day__counter">1</span>
+      <time class="day__date" datetime="2019-03-18">MAR 18</time>
+    </div>
+    </li>`
+  );
+};
+
+const createTripDaysItem = () => {
+  return (
+    `<ul class="trip-events__list"></ul>`
+  );
+};
+
+const createTripEvent = () => {
   return (
 
-    `<ul class="trip-days">
-            <li class="trip-days__item  day">
-              <div class="day__info">
-                <span class="day__counter">1</span>
-                <time class="day__date" datetime="2019-03-18">MAR 18</time>
-              </div>
+    `<li class="trip-events__item">
+      <div class="event">
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">Taxi to Amsterdam</h3>
 
-              <ul class="trip-events__list">
-                <li class="trip-events__item">
-                  <div class="event">
-                    <div class="event__type">
-                      <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
-                    </div>
-                    <h3 class="event__title">Taxi to Amsterdam</h3>
+        <div class="event__schedule">
+        <p class="event__time">
+        <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+        &mdash;
+        <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+        </p>
+        <p class="event__duration">30M</p>
+      </div>
 
-                    <div class="event__schedule">
-                      <p class="event__time">
-                        <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
-                        &mdash;
-                        <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
-                      </p>
-                      <p class="event__duration">30M</p>
-                    </div>
+      <p class="event__price">
+      &euro;&nbsp;<span class="event__price-value">20</span>
+      </p>
 
-                    <p class="event__price">
-                      &euro;&nbsp;<span class="event__price-value">20</span>
-                    </p>
+      <h4 class="visually-hidden">Offers:</h4>
+      <ul class="event__selected-offers">
+        <li class="event__offer">
+          <span class="event__offer-title">Order Uber</span>&plus;
+          &euro;&nbsp;<span class="event__offer-price">20</span>
+        </li>
+      </ul>
 
-                    <h4 class="visually-hidden">Offers:</h4>
-                    <ul class="event__selected-offers">
-                      <li class="event__offer">
-                        <span class="event__offer-title">Order Uber</span>
-                        &plus;
-                        &euro;&nbsp;<span class="event__offer-price">20</span>
-                       </li>
-                    </ul>
-
-                    <button class="event__rollup-btn" type="button">
-                      <span class="visually-hidden">Open event</span>
-                    </button>
-                  </div>
-                </li>
-              </ul>
-            </li>
-          </ul>`
+      <button class="event__rollup-btn" type="button">
+        <span class="visually-hidden">Open event</span>
+      </button>
+    </div>
+  </li>`
 
   );
 };
@@ -345,9 +356,16 @@ render(headerMenuElement, createTripFilters(), `beforeend`);
 const mainSortElement = document.querySelector(`.trip-events`);
 render(mainSortElement, createTripSort(), `beforeend`);
 render(mainSortElement, createEventEdit(), `beforeend`);
+render(mainSortElement, createTripDaysList(), `beforeend`);
+
+const tripDaysListElement = mainSortElement.querySelector(`.trip-days`);
+render(tripDaysListElement, createTripDays(), `beforeend`);
+
+const tripDaysElement = mainSortElement.querySelector(`.trip-days__item`);
+render(tripDaysElement, createTripDaysItem(), `beforeend`);
+
+const tripDaysItem = mainSortElement.querySelector(`.trip-events__list`);
 
 for (let i = 0; i < COUNT; i++) {
-  render(mainSortElement, createSiteTripDays(), `beforeend`);
+  render(tripDaysItem, createTripEvent(), `beforeend`);
 }
-
-
