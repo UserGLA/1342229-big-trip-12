fc04403b3fdd59463ac281e62644a23c82da72b2
UserGLA <tigr1488@yandex.ru>
2020-09-16 02:36:55 +0700

    Поправил ошибки
    Также сделал вторую часть (11. Личный проект: революция или эволюция? (Часть 2) в этой ветке (забыл переключить)

--- src/main.js
+++ src/main.js
@@ -6,12 +6,51 @@ import TripSort from "./view/trip-sort.js";
 import TripDaysList from "./view/trip-daysList.js";
 import TripDay from "./view/trip-day.js";
 import TripEvent from "./view/trip-event.js";
+/*
+import EventNew from "./view/event-new.js";
+*/
 import EventEdit from "./view/event-edit.js";
+import NoEvent from "./view/no-event.js";
 
 import {events} from "./mock/event.js";
 import {renderElement, RenderPosition} from "./utils.js";
 
 const dates = [...new Set(events.map((item) => new Date(item.startDate).toDateString()))];
+
+const renderEvent = (eventElement, _event) => {
+  const eventComponent = new TripEvent(_event);
+  const eventEditComponent = new EventEdit(_event);
+  const onEscKeyDown = (evt) => {
+    if (evt.key === `Escape` || evt.key === `Esc`) {
+      evt.preventDefault();
+      replaceFormToCard();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
+  const replaceCardToForm = () => {
+    eventElement.replaceChild(eventEditComponent.getElement(), eventComponent.getElement());
+  };
+
+  const replaceFormToCard = () => {
+    eventElement.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
+  };
+
+  eventComponent.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, () => {
+    replaceCardToForm();
+    document.addEventListener(`keydown`, onEscKeyDown);
+  });
+
+  eventEditComponent.getElement().querySelector(`.event__reset-btn`).addEventListener(`click`, () => {
+    replaceFormToCard();
+    document.addEventListener(`keydown`, onEscKeyDown);
+  });
+  eventEditComponent.getElement().querySelector(`.event__save-btn`).addEventListener(`click`, () => {
+    replaceFormToCard();
+    document.addEventListener(`keydown`, onEscKeyDown);
+  });
+
+  renderElement(eventElement, eventComponent.getElement(), RenderPosition.BEFOREEND);
+};
 
 const headerMainElement = document.querySelector(`.trip-main`);
 
@@ -20,32 +59,12 @@ renderElement(headerMainElement.querySelector(`.trip-main__trip-info`), new Trip
 renderElement(headerMainElement.querySelector(`.trip-controls`), new TripMenu().getElement(), RenderPosition.AFTERBEGIN);
 renderElement(headerMainElement.querySelector(`.trip-controls`), new TripFilters().getElement(), RenderPosition.BEFOREEND);
 renderElement(document.querySelector(`.trip-events`), new TripSort().getElement(), RenderPosition.BEFOREEND);
-renderElement(document.querySelector(`.trip-events`), new TripDaysList().getElement(), RenderPosition.BEFOREEND);
 
-const renderEvent = (EventElement, _event) => {
-  const EventComponent = new TripEvent(_event);
-  const EventEditComponent = new EventEdit(_event);
-  const replaceCardToForm = () => {
-    EventElement.replaceChild(EventEditComponent.getElement(), EventComponent.getElement());
-  };
-
-  const replaceFormToCard = () => {
-    EventElement.replaceChild(EventComponent.getElement(), EventEditComponent.getElement());
-  };
-
-  EventComponent.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, () => {
-    replaceCardToForm();
-  });
-
-  EventEditComponent.getElement().querySelector(`.event__reset-btn`).addEventListener(`click`, () => {
-    replaceFormToCard();
-  });
-  EventEditComponent.getElement().querySelector(`.event__save-btn`).addEventListener(`click`, () => {
-    replaceFormToCard();
-  });
-
-  renderElement(EventElement, EventComponent.getElement(), RenderPosition.BEFOREEND);
-};
+if (events.length === 0) {
+  renderElement(document.querySelector(`.trip-events`), new NoEvent().getElement(), RenderPosition.BEFOREEND);
+} else {
+  renderElement(document.querySelector(`.trip-events`), new TripDaysList().getElement(), RenderPosition.BEFOREEND);
+}
 
 dates.forEach((date, dateIndex) => {
   const day = new TripDay(new Date(date), dateIndex + 1).getElement();
